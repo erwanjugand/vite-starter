212ad5782b454f39a30cf354e802fbdc1d08b876
Erwan Jugand <erwan.jugand@gmail.com>
2022-05-02 22:03:41 +0200

    init

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  entry: './src/index.ts',
+
+  output: {
+    filename: 'script.js',
+    clean: true
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: [
+          'ts-loader'
+        ]
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader'
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [
+                  ['autoprefixer']
+                ]
+              }
+            }
+          },
+          {
+            loader: 'sass-loader'
+          }
+        ],
+      }
+    ]
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.css'
+    })
+  ],
+}
