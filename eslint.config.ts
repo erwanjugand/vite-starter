b2993ce616c1670c92477d07bd5db9fce5d309f5
Erwan Jugand <erwan.jugand@gmail.com>
2025-07-19 20:16:43 +0200

    chore(prettier): update import

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,6 +1,6 @@
 import eslint from '@eslint/js'
 import tsConfig from 'typescript-eslint'
-import prettierConfig from 'eslint-config-prettier'
+import prettierConfig from 'eslint-config-prettier/flat'
 
 export default tsConfig.config(
   eslint.configs.recommended,
