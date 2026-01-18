8fc6810c42c1d09da24bba611808795f86cae7a2
Erwan Jugand <erwan.jugand@gmail.com>
2026-01-18 11:49:43 +0100

    refactor config

---
+++ stylelint.config.js
@@ -0,0 +1,5 @@
+/** @type {import('stylelint').Config} */
+// oxlint-disable-next-line no-anonymous-default-export
+export default {
+  extends: ['stylelint-config-recess-order', 'stylelint-config-standard-scss'],
+}
