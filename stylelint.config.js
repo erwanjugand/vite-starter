438f019cbff0a713d7510c6df3d93ab461d1c2a6
Erwan Jugand <erwan.jugand@gmail.com>
2026-05-09 19:55:39 +0200

    chore(stylelint): migrate config

--- stylelint.config.js
+++
@@ -1,5 +0,0 @@
-/** @type {import('stylelint').Config} */
-// oxlint-disable-next-line no-anonymous-default-export
-export default {
-  extends: ['stylelint-config-recess-order', 'stylelint-config-standard-scss'],
-}
